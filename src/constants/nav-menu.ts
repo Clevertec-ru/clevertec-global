abbee7f934c30de48e7568a6973875deabcf3675
klichkovskiy <klichkouski@gmail.com>
2023-09-15 14:43:59 +0300

    feat(*): сделать блок nav-menu

---
+++ src/constants/nav-menu.ts
@@ -0,0 +1,21 @@
+export const NAV_MENU = {
+    logoTitle: 'WislaCode Solutions',
+    menu: [
+        {
+            text: 'About',
+            link: '/#about'
+        },
+        {
+            text: 'What we do',
+            link: '/#what-we-do'
+        },
+        {
+            text: 'Cases',
+            link: '/#cases'
+        },
+        {
+            text: 'Contact us',
+            link: '/#contact-us'
+        }
+    ]
+}
---
+++ src/hooks/useScrollPosition.js
@@ -0,0 +1,42 @@
+import { useRef, useLayoutEffect, useEffect } from 'react';
+
+const isBrowser = typeof window !== `undefined`;
+
+const getScrollPosition = ({ element, useWindow }) => {
+    if (!isBrowser) return { x: 0, y: 0 };
+
+    const target = element ? element.current : document.body;
+    const position = target.getBoundingClientRect();
+
+    return useWindow ? { x: window.scrollX, y: window.scrollY } : { x: position.left, y: position.top };
+};
+
+export function useScrollPosition(effect, deps, element, useWindow, wait) {
+    const position = useRef(getScrollPosition({ useWindow }));
+    const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
+
+    let throttleTimeout = null;
+
+    const callBack = () => {
+        const currPos = getScrollPosition({ element, useWindow });
+        effect({ prevPos: position.current, currPos });
+        position.current = currPos;
+        throttleTimeout = null;
+    };
+
+    useIsomorphicLayoutEffect(() => {
+        const handleScroll = () => {
+            if (wait) {
+                if (throttleTimeout === null) {
+                    throttleTimeout = setTimeout(callBack, wait);
+                }
+            } else {
+                callBack();
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, deps);
+}
